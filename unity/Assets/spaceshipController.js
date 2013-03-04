fb853166f8603db285288be455f5efb3c1020a0d
Lara Formosa <laraformosa@gmail.com>
2013-03-04 10:31:09 +0100

    lives and collision

--- unity/Assets/spaceshipController.js
+++ unity/Assets/spaceshipController.js
@@ -1,8 +1,11 @@
 #pragma strict
 var speed:int;
 var rotation: int;
+var lives: int;
 
-function Start () {
+function Start () 
+{
+lives=1;
 
 }
 
@@ -11,7 +14,7 @@ var leftmost:float = Camera.main.ScreenToWorldPoint(Vector3(0, 0, 0)).x;
 var topmost:float = Camera.main.ScreenToWorldPoint(Vector3(0, 0, 0)).y;
 var rightmost:float = Camera.main.ScreenToWorldPoint(Vector3(Screen.width, Screen.height, 0)).x;
 var bottommost:float = Camera.main.ScreenToWorldPoint(Vector3(Screen.width, Screen.height, 0)).y;
-
+// .left since the image was originally on the left, if it was pointing up we would have changed it to up
 transform.Translate(Vector3.left* speed* Input.GetAxis ("Vertical")* Time.deltaTime);
 transform.Rotate(Vector3.back* rotation* Input.GetAxis ("Horizontal")* Time.deltaTime);
 
@@ -32,4 +35,24 @@ if (transform.position.y>bottommost)
 	transform.position.y=topmost;
 }
 
+if (lives==0)
+{
+	Destroy(this.gameObject);
+}
+
+}
+
+function OnTriggerEnter(other:Collider)
+{
+	if (other.gameObject.tag=="TagAsteroid")
+	{
+		lives--;
+	}
+	
+	Debug.Log(other.gameObject.tag);
+}
+
+function OnGUI()
+{
+	GUI.Label(Rect(5,5,50,25),"Lives: "+lives);
 }
